21e5e32d713c003b51d4572c7d51a27b72c45007
IvanJevtic9 <ijevtic459@gmail.com>
2020-05-22 18:01:31 +0200

    22.5

--- src/app/ui/admin/admin.component.ts
+++ src/app/ui/admin/admin.component.ts
@@ -1,12 +1,12 @@
 import { MenuItem } from 'primeng/api';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
-export class AdminComponent implements OnInit {
+export class AdminComponent implements OnInit, AfterViewInit {
   items: MenuItem[];
   constructor() { }
 
@@ -27,11 +27,38 @@ export class AdminComponent implements OnInit {
           {label: 'New', icon: 'pi pi-fw pi-plus'},
           {label: 'Download', icon: 'pi pi-fw pi-download', items: [
             {label: 'New', routerLink: 'orders', icon: 'pi pi-fw pi-plus'},
-            {label: 'Download', icon: 'pi pi-fw pi-download'}
+            {
+              label: 'Download',
+              icon: 'pi pi-fw pi-download',
+              items: [
+                {label: 'New', icon: 'pi pi-fw pi-plus'},
+                {label: 'Download', icon: 'pi pi-fw pi-download', items: [
+                  {label: 'New', routerLink: 'orders', icon: 'pi pi-fw pi-plus'},
+                  {label: 'Download', icon: 'pi pi-fw pi-download'}
+                ]}
+              ]
+            }
           ]}
         ]
       }
     ];
   }
-
+  ngAfterViewInit() {
+    const buttons = document.querySelectorAll('a');
+    console.log(buttons);
+    buttons.forEach(btn => {
+      btn.addEventListener('mousedown', (e) => {
+        let x = e.clientX - e.target.offsetLeft;
+        const y = e.clientY - e.target.offsetTop;
+        const ripples = document.createElement('span');
+        ripples.className = 'ripple';
+        ripples.style.left = x + 'px';
+        ripples.style.top = y + 'px';
+        btn.appendChild(ripples);
+        setTimeout(() => {
+          ripples.remove();
+        }, 400);
+      });
+    });
+  }
 }
